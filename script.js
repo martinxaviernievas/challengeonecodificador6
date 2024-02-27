bb10d2cba769623007fe29789e56930e821630a3
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2024-02-26 23:23:51 -0300

    Finalizado encriptador

---
+++ script.js
@@ -0,0 +1,68 @@
+var botonEncriptar = document.querySelector(".btn-encrypt");
+var botonDesencriptar = document.querySelector(".btn-decrypt");
+var picture = document.querySelector(".content-picture");
+var contenedor = document.querySelector(".content-parrafo");
+var resultado = document.querySelector(".content-result");
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+
+function encriptar(){
+    ocultarAdelante();
+    var boxtext = recuperarTexto()
+    resultado.textContent = encriptarTexto(boxtext);
+}
+
+function desencriptar(){
+    ocultarAdelante();
+    var boxtext = recuperarTexto()
+    resultado.textContent = desencriptarTexto(boxtext);
+}
+
+function recuperarTexto(){
+    var boxtext = document.querySelector(".boxtext")
+    return boxtext.value
+}
+
+function ocultarAdelante(){
+    picture.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje) {
+    const vocales = {
+        a: "ai",
+        e: "enter",
+        i: "imes",
+        o: "ober",
+        u: "ufat"
+    };
+
+    mensaje = mensaje.toLowerCase(); // Convertir a minúsculas
+
+    let textoFinal = "";
+    for (let i = 0; i < mensaje.length; i++) {
+        textoFinal += vocales[mensaje[i]] || mensaje[i];
+    }
+    return textoFinal;
+}
+
+function desencriptarTexto(mensaje) {
+    mensaje = mensaje.toLowerCase(); // Convertir a minúsculas
+
+    var textoDesencriptado = mensaje.replace(/ai/g, "a")
+                                    .replace(/enter/g, "e")
+                                    .replace(/imes/g, "i")
+                                    .replace(/ober/g, "o")
+                                    .replace(/ufat/g, "u");
+
+    return textoDesencriptado;
+}
+
+  
+  var btnCopy = document.querySelector(".btn-copy");
+  btnCopy.addEventListener("click", copiar = () => {
+      var contenido = document.querySelector(".content-result").textContent;
+      navigator.clipboard.writeText(contenido);
+      console.log("hola");
+  });
